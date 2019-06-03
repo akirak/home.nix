e25953a172d46141958c91b216f330b0065dcb2e
Akira Komamura <akira.komamura@gmail.com>
2019-06-03 18:50:53 +0900

    Prevent from installing tilix on WSL
    because it fails to build.

--- apps/default.nix
+++ apps/default.nix
@@ -5,7 +5,7 @@ with profile.preferences;
   (import ./emacs.nix attrs)
   (import ./zsh.nix attrs)
   (import ./graphical.nix attrs)
-  (import ./tilix.nix attrs)
+  (optionalAttrs (!profile.platform.isWsl) (import ./tilix.nix attrs))
   (optionalAttrs profile.platform.isWayland (import ./wayland.nix attrs))
   (import ./org-personal.nix attrs)
   (import ./kbfs.nix attrs)
