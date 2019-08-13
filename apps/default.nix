8918b248caa2fa14a1eaa86dbbcf8d9f5478e1b3
Akira Komamura <akira.komamura@gmail.com>
2019-08-13 22:46:46 +0900

    Revert "Disable tilix"
    This reverts commit bbcefa7d0371caa8d6a441b0a43c5f9b6a4cf0f7.
    
    I thought Alacritty would suffice, but it turns out it does not run on
    CloudReady on stable channel, because it disables GPU acceleration.
    
    For CloudReady on stable, I need another terminal emulator application
    that does not require GPU acceleration.

--- apps/default.nix
+++ apps/default.nix
@@ -5,7 +5,7 @@ with profile.preferences;
   (import ./emacs.nix attrs)
   (import ./zsh.nix attrs)
   (import ./graphical.nix attrs)
-  # (optionalAttrs (!profile.platform.isWsl) (import ./tilix.nix attrs))
+  (optionalAttrs (!profile.platform.isWsl) (import ./tilix.nix attrs))
   (import ./alacritty.nix attrs)
   (optionalAttrs profile.platform.isChromeOS (import ./garcon.nix attrs))
   (optionalAttrs profile.platform.isWayland (import ./wayland.nix attrs))
