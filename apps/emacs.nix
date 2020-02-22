80a1c63958b9095355d268cb1b267d508661e3ff
Akira Komamura <akira.komamura@gmail.com>
2020-02-22 15:57:12 +0900

    Add liberime

--- apps/emacs.nix
+++ apps/emacs.nix
@@ -15,6 +15,7 @@ with profile.path;
         elisp-ffi
         exwm
         beancount
+        liberime-config
       ];
     overrides = self: super: {
       mozc = with pkgs;
@@ -98,7 +99,44 @@ with profile.path;
 '';
       };
 
-     };
+      # based on the configuration of vterm
+      liberime-config = self.melpaBuild rec {
+        pname = "liberime-config";
+        version = "0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "merrickluo";
+          repo = "liberime";
+          rev = "282d37c44b9bda33bba2b6a23bd4b2ab5807ed15";
+          # date = 2020-01-12T14:39:55+08:00;
+          sha256 = "0s87pq3cp4wgvh14hjqcrr059c5sxrmb631x7008j1cy5lyn09z0";
+        };
+
+        recipe = pkgs.writeText "recipe" ''
+          (liberime-config :fetcher github :repo "merrickluo/liberime")
+        '';
+
+        buildInputs = [ self.emacs pkgs.cmake pkgs.librime ];
+
+        # cmakeFlags = [
+        #   "-DEMACS_SOURCE=${self.emacs.src}"
+        # ];
+
+        # we need the proper out directory to exist, so we do this in the
+        # postInstall instead of postBuild
+        postInstall = ''
+            cd $src
+            cp -r -t $out src CMakeLists.txt Makefile
+            cd $out
+            make
+            cd share/emacs/site-lisp/elpa/liberime-config-**
+            mkdir build
+            install -m 444 -v -t build $out/build/liberime.so
+            # ls -l build
+            # rm -rf $out/share/emacs/site-lisp/elpa/vterm-**/{CMake*,build,*.c,*.h}
+          '';
+      };
+    };
   };
 
   home.packages = with pkgs; [
@@ -153,6 +191,9 @@ with profile.path;
     hunspellDicts.en-us
     hunspellDicts.en-gb-ise
     # hunspellDicts.en-gb-ize
+    # Data for rime (Chinese input)
+    # This somehow causes conflicts. I will review it later.
+    # brise
   ];
 
   home.sessionVariables = {
