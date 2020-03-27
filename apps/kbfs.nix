c561498039e2d597c8720691ed8a4bc09efeeabf
Akira Komamura <akira.komamura@gmail.com>
2020-03-28 04:40:04 +0900

    apps/kbfs.nix: Use the default kbfs service unit

--- apps/kbfs.nix
+++ apps/kbfs.nix
@@ -1,12 +1,7 @@
 { profile, pkgs, lib, desktop, ... }:
 with profile.path;
-let
-  runtimeDir = builtins.getEnv "XDG_RUNTIME_DIR";
-  mountPoint = "${runtimeDir}/keybase/kbfs";
-in
 {
   home.packages = with pkgs; [
-    kbfs
     keybase
     keybase-gui
   ];
@@ -24,49 +19,9 @@ in
     enable = true;
   };
 
-  # An alternative service unit for kbfs.
-  #
-  # To use this service, you have to take the following additional steps:
-  #
-  # 1. Install a package that provides =fusermout=. On Debian,
-  # this is fuse package. Note that fusermount 2.9.9 from nixpkgs
-  # doesn't seem to work, while fusermount 2.9.7 from the Debian
-  # repo works.
-  #
-  # 2. Create /keybase directory owned by the user.
-  systemd.user.services.kbfs =
-    {
-      Unit = {
-        Description = "Keybase File System";
-        Requires = [ "keybase.service" ];
-        Wants = [
-          "kbfs-root.service"
-        ];
-        After = [
-          "kbfs-root.service"
-          "keybase.service"
-        ];
-
-       # Use fusermount provided by the OS distribution
-        AssertFileIsExecutable = "/bin/fusermount";
-      };
-
-      Service = {
-        Environment = [
-          "KEYBASE_SYSTEMD=1"
-        ];
-        ExecStartPre = "/bin/mkdir -p ${mountPoint}";
-        ExecStart ="${pkgs.kbfs}/bin/kbfsfuse ${mountPoint}";
-        # Use fusermount provided by the OS distribution
-        ExecStopPost = "/bin/fusermount -u ${mountPoint}";
-        Restart = "on-failure";
-        PrivateTmp = true;
-      };
-
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-    };
+  services.kbfs = {
+    enable = true;
+  };
 
   systemd.user.services.kbfs-root =
     {
@@ -76,7 +31,11 @@ in
 
       Service = {
         Type = "oneshot";
-        ExecStart ="/bin/sh -c '[ -e /keybase ] || sudo ln -sf ${mountPoint} /keybase'";
+        ExecStart ="/bin/sh -c '[ -e /keybase ] || sudo ${pkgs.coreutils}/bin/ln -sf $HOME/keybase /keybase'";
+      };
+
+      Install = {
+        WantedBy = [ "kbfs.service" ];
       };
     };
 
