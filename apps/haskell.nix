419974ea00d1360f2f3e80be678ee573d6e5c7b3
Akira Komamura <akira.komamura@gmail.com>
2020-07-17 00:10:02 +0900

    apps/haskell.nix: Add extra packages for Haskell

--- apps/haskell.nix
+++ apps/haskell.nix
@@ -1,5 +1,10 @@
 { profile, pkgs, lib, desktop, ... }:
 with profile.path; {
+  home.packages = with pkgs.haskellPackages; [
+    structured-haskell-mode
+    hoogle
+  ];
+
   home.file.".stack/config.yaml".text =
     ''
     nix:
