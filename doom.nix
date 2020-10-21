53e751583adda8372c83f018b895ffefb48e3caf
Akira Komamura <akira.komamura@gmail.com>
2020-10-22 00:37:09 +0900

    Add nix-shell config for using Doom

---
+++ doom.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import (import ./nix/sources.nix).nixpkgs-unstable {
+} }:
+with pkgs;
+with builtins;
+let
+  configDir = "${builtins.getEnv "HOME"}/.config/doom-runner";
+in
+mkShell {
+  XDG_CONFIG_HOME = configDir;
+
+  buildInputs = [
+    emacs
+  ];
+
+  shellHook = ''
+    if [[ -f "$HOME/.emacs" || -d "$HOME/.emacs.d" ]]; then
+      echo "Non-XDG user-emacs-directory is getting in the way."
+      exit 1
+    fi
+
+    configDir="''${XDG_CONFIG_HOME}"
+    doomDir="$configDir/emacs"
+    privateDir="$configDir/doom"
+
+    mkdir -p $configDir
+    if ! [[ -d $doomDir ]]; then
+      git clone --depth 1 https://github.com/hlissner/doom-emacs $doomDir
+    fi
+
+    export PATH="$doomDir/bin:$PATH"
+
+    if ! [[ -d $privateDir ]]; then
+      echo
+      echo "The installation of Doom has not been completed yet."
+      echo
+      echo "If you have your own private config, clone it to $privateDir."
+      echo
+      echo "Run 'doom install' before you start Emacs for the first time."
+      echo
+    fi
+  '';
+}
