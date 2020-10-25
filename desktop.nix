1822ce34a5b0824d39c394284fa0358f069cc465
Akira Komamura <akira.komamura@gmail.com>
2020-10-26 01:12:50 +0900

    Add desktop.nix for nix-desktop

---
+++ desktop.nix
@@ -0,0 +1,17 @@
+# Configuration file for nix-desktop
+# https://github.com/akirak/nix-desktop
+#
+# You can install desktop files by running `nix-desktop install .`
+let
+  pkgs = import <nixpkgs> {};
+in
+{
+  name = "my-config";
+  applications.doom-emacs = {
+    Name = "Doom Emacs";
+    Icon = "emacs";
+    TryExec = "${builtins.getEnv "HOME"}/.config/doom-runner/emacs/bin/doom";
+    Exec = "${pkgs.nix}/bin/nix-shell ${builtins.toString ./.}/doom/shell.nix --command emacs";
+    StartupWMClass = "Emacs";
+  };
+}
