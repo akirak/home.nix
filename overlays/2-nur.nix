dbaa0cd91da17cf2e79f4bb20e3db61fb6b15f4a
Akira Komamura <akira.komamura@gmail.com>
2020-12-02 01:16:32 +0900

    Update my NUR

--- overlays/2-nur.nix
+++ overlays/2-nur.nix
@@ -13,9 +13,9 @@ in
     akirak = import (super.fetchFromGitHub {
       owner = "akirak";
       repo = "nur-packages";
-      rev = "e0362a3048144fe671fe356ebae35e393cf66985";
-      # date = 2020-12-02T00:50:14+09:00;
-      sha256 = "1yrk2vqjxqy11bplmw477p4sw21imgq0n67qh7a8ispk75dwrgz0";
+      rev = "5dab4bfb44e7526b5deb017175b8ec184cde6cda";
+      # date = 2020-12-02T01:14:34+09:00;
+      sha256 = "1qhn0yaql7zknmrifp6pdn5bj54hn291j0shl92fjdcqrdvbqnbz";
     }) { pkgs = super; };
   };
 }
