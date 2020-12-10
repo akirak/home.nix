f6faf1244a70fe6942d85bb4cb9db565bc38e50f
Akira Komamura <akira.komamura@gmail.com>
2020-12-10 11:32:52 +0900

    Clean up

--- home.nix
+++ home.nix
@@ -1,19 +1,47 @@
 { pkgs, lib, ... }:
 with lib;
 let
-  profile = import ./functions/profile.nix rec {
-    profile = import ./profile.nix {};
-    path =
-      rec {
-        homeDirectory = builtins.getEnv "HOME";
-        channelsDir = "${homeDirectory}/.nix-defexpr/channels";
-        binDir = "${homeDirectory}/.nix-profile/bin";
-        nixBinDir =
-          if profile.platform.isNixOS
-          then "/run/current-system/sw/bin"
-          else binDir;
+  profile =
+    let
+      mkProfile = x: rec {
+        # You have to configure an identity
+        github = { user = "akirak"; } // (x.github or {});
+        language = {
+          base = "en";
+        } // (x.language or {});
+        # This doesn't seem to take effect, so I will set locales directly
+        # in sessionVariables.
+        locale = x.locale or {};
+        preferences = {
+          useBrowserPass = true;
+          addGlobalGitIdentity = false;
+        } // (x.preferences or {});
+        platform =
+          let
+            that = x.platform;
+          in rec {
+            isWsl = that.isWsl or false;
+            isChromeOS = that.isChromeOS or false;
+            isNixOS = that.isNixOS or false;
+            isWayland = that.isWayland or isChromeOS;
+          };
       };
-  };
+    in
+      (mkProfile (import ./profile.nix {})) //
+    {
+      identity = import ~/local/identity.nix;
+
+      path =
+        rec {
+          homeDirectory = builtins.getEnv "HOME";
+          channelsDir = "${homeDirectory}/.nix-defexpr/channels";
+          binDir = "${homeDirectory}/.nix-profile/bin";
+          nixBinDir =
+            if profile.platform.isNixOS
+            then "/run/current-system/sw/bin"
+            else binDir;
+        };
+    };
   extendConfigWith = import ./functions/extend-config.nix { inherit lib; };
   desktop = import ./functions/desktop.nix;
   attrs = { inherit profile pkgs lib desktop; };
