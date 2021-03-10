abf1765a3218938fb82c9fc9e8476b2ff7788bc1
Akira Komamura <akira.komamura@gmail.com>
2021-03-11 00:42:57 +0900

    Update NUR

--- overlays/2-nur.nix
+++ overlays/2-nur.nix
@@ -3,9 +3,9 @@ let
   global = super.fetchFromGitHub {
     owner = "nix-community";
     repo = "NUR";
-    rev = "2b4c62062d656f58baf46d19462d526638e4dda9";
-    sha256 = "00c5m5dj5li0y0f8mzdsaykcnpkppj6sgmbjkliszg6wi0dqidpj";
-    # date = 2020-12-01T08:36:42+00:00;
+    rev = "2f0d93976702d07e984a836865af217d1bb496c8";
+    sha256 = "1iicqs6jqn4s2nh5lqg3pzmz2k3nmlzmn19i4liksr7lc83frb55";
+    # date = 2021-03-10T14:40:23+00:00;
   };
 in
 {
@@ -13,9 +13,9 @@ in
     akirak = import (super.fetchFromGitHub {
       owner = "akirak";
       repo = "nur-packages";
-      rev = "c19d9b88a00447ce014a60bbf9a7b6007e36317f";
-      # date = 2020-12-10T05:01:11+09:00;
-      sha256 = "1qac2zck0bjckmcflgni5jv20ydv08f33v28wni6gbcgmdhjqh1x";
+      rev = "33b61e4a8531b48f5e421c8a07f3e13ada619c27";
+      # date = 2021-03-11T00:33:27+09:00;
+      sha256 = "0fq60q93lqkxs2dlz4jdwln9nxvdrv6hlwhqdicbidvzwbd3rjbz";
     }) { pkgs = super; };
   };
 }
